cae1e4d8047007bef9c79869a739a993aee1c850
زياد محمود السيد مصطفي <88041262+ZiadSakr1@users.noreply.github.com>
2024-06-22 08:15:22 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image as keras_image
+import h5py
 
 app = FastAPI()
 
